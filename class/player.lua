591203be696572726aa91a4be3b9b678f9f9661a
Britdan <alwaysinthedark44@gmail.com>
2025-03-14 19:51:39 +0000

    simple auto moving character and chip placer

--- class/player.lua
+++ class/player.lua
@@ -1,17 +1,34 @@
 local player = Class("player",OBJECTS.box)
 function player:initialize(world,x,y,w,h)
     OBJECTS.box.initialize(self,world,x,y,w,h,{gravity=128})
+    self.started = false
 end
 
 function player:update(dt)
-    self.VX = 0
-    local speed = 32
-    if INPUT:Holding("left")  then self.VX = -speed end
-    if INPUT:Holding("right") then self.VX = speed end
+    if not self.started then return end
+    local tilex, tiley = GetTileAtPos(self.X+15,self.Y+15)
+    if tilex ~= self.lastTile.X or tiley ~= self.lastTile.Y then
+        self.lastTile = {X=tilex,Y=tiley}
+        local chip = GetChipAtTile(tilex,tiley)
+        if chip then
+            if chip.action == "jump" then
+                self.VY = -96
+            end
+            if chip.action == "move_left" then
+                self.VX = -40
+            end
+            if chip.action == "move_right" then
+                self.VX = 40
+            end
+        end
+    end
 end
 
-function player:jump()
-    self.VY = -96
+function player:start()
+    self.VX = 40
+    local tilex, tiley = GetTileAtPos(self.X+15,self.Y+15)
+    self.lastTile = {X=tilex,Y=tiley}
+    self.started = true
 end
 
 OBJECTS.player = player
--- scenes/game.lua
+++ scenes/game.lua
@@ -4,7 +4,9 @@ local loadobject = function(data)
     if data.class == "ground" then
         return OBJECTS.ground:new(GAME.WORLD,data.X,data.Y,data.W,data.H,data.args)
     elseif data.class == "player" then
-        return OBJECTS.player:new(GAME.WORLD,data.X,data.Y,16,16)
+        local player = OBJECTS.player:new(GAME.WORLD,data.X,data.Y,16,16)
+        if not GAME.PLAYER then GAME.PLAYER = player end
+        return player
     end
 end
 
@@ -12,9 +14,13 @@ GAME = {}
 function scene.LoadScene()
     GAME.WORLD = BUMP.newWorld(16)
     GAME.MAP = MAP:new("maps/test.lua",{LoadObject=loadobject})
-    table.insert(GAME.MAP:GetLayer("Objects").objects,OBJECTS.player:new(GAME.WORLD,0,0,16,16))
-
+    table.insert(GAME.MAP:GetLayer("Objects").objects,OBJECTS.ground:new(GAME.WORLD,-16,-16,288,16,{}))
+    table.insert(GAME.MAP:GetLayer("Objects").objects,OBJECTS.ground:new(GAME.WORLD,-16,0,16,256,{}))
+    table.insert(GAME.MAP:GetLayer("Objects").objects,OBJECTS.ground:new(GAME.WORLD,256,0,16,256,{}))
+    table.insert(GAME.MAP:GetLayer("Objects").objects,OBJECTS.ground:new(GAME.WORLD,-16,256,288,16,{}))
     GAME.MAPPOS = {X=7,Y=7}
+
+    GAME.CHIPS = {}
 end
 function scene.Update(dt)
     GAME.MAP:GetLayer("Objects"):LoopThrough(function(data)
@@ -31,14 +37,48 @@ function scene.Draw()
     GAME.MAP:GetLayer("Objects"):LoopThrough(function(data)
         love.graphics.setColor(1,0.3,0.3)
         love.graphics.rectangle("line",GAME.MAPPOS.X+data.obj.X,GAME.MAPPOS.Y+data.obj.Y,data.obj.W,data.obj.H)
+        if data.obj.started then
+            love.graphics.setColor(1,1,1)
+            love.graphics.rectangle("fill",GAME.MAPPOS.X+data.obj.X+7,GAME.MAPPOS.Y+data.obj.Y+7,2,2)
+        end
     end)
+
+    for i,v in pairs(GAME.CHIPS) do
+        love.graphics.setColor(v.color)
+        love.graphics.rectangle("line",(v.X+(GAME.MAPPOS.X/16))*16,(v.Y+(GAME.MAPPOS.Y/16))*16,16,16)
+    end
 end
 
-function scene.InputPressed(name,dir)
-    if name == "jump" then
-        GAME.MAP:GetLayer("Objects"):LoopThrough(function(data)
-            if data.obj.jump then data.obj:jump() end
-        end)
+function GetTileAtPos(x,y)
+    return math.floor((x-GAME.MAPPOS.X)/16), math.floor((y-GAME.MAPPOS.Y)/16)
+end
+
+function GetChipAtTile(x,y)
+    for i,v in pairs(GAME.CHIPS) do
+        if v.X == x and v.Y == y then
+            return v, i
+        end
+    end
+end
+
+function scene.Mousepressed(mx,my,b)
+    local tilex, tiley = GetTileAtPos(mx,my)
+    local _,chipi = GetChipAtTile(tilex,tiley)
+    if chipi then
+        table.remove(GAME.CHIPS,chipi); return
+    end
+    if b == 1 then
+        table.insert(GAME.CHIPS,{X=tilex,Y=tiley,action="jump",color={0,1,0}})
+    elseif b == 2 then
+        table.insert(GAME.CHIPS,{X=tilex,Y=tiley,action="move_left",color={1,0,0}})
+    elseif b == 3 then
+        table.insert(GAME.CHIPS,{X=tilex,Y=tiley,action="move_right",color={0,0,1}})
+    end
+end
+
+function scene.Keypressed(key)
+    if key == "space" then
+        GAME.PLAYER:start()
     end
 end
 
