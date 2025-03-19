46fb2473c034be252a01e6b95d3de1b23aabfcdd
Britdan <alwaysinthedark44@gmail.com>
2025-03-19 19:27:39 +0000

    New title and name everywhere

--- conf.lua
+++ conf.lua
@@ -2,7 +2,8 @@ function love.conf(t)
     local scale = 4
     Env = {windowwidth=(1920/4)*scale, windowheight=(1088/4)*scale, scale=scale}
     Env.width, Env.height = Env.windowwidth/Env.scale, Env.windowheight/Env.scale
-    t.identity = "BritBase"
+    t.identity = "Pre-Meditated"
+    t.window.title = "Pre-Meditated"
     t.window.icon = "graphics/icon.png"
 
     t.window.width = Env.windowwidth
